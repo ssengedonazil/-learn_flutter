089853abd7904cc21a083da67c6b4c4239cc7568
sengendo Nazil <ssengendonazil@gmail>
2025-07-26 01:41:38 +0300

    dialog

--- lib/Views/Pages/well_come.dart
+++ lib/Views/Pages/well_come.dart
@@ -18,12 +18,28 @@ class _WellComePageState extends State<WellComePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-           HeroWidget(),
+            FittedBox(child: Text("Flutter App", style: TextStyle(letterSpacing: 50.0,fontSize: 40.0, fontWeight: FontWeight.bold),),
+            
+            ),
+          //  HeroWidget(),
             FilledButton(onPressed: (){
                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)  {
                   return WidgetTree();
                 }));
-            }, child: Text("Login"))
+            },
+            style: FilledButton.styleFrom(
+              minimumSize: Size(double.infinity, 40.0)
+            ),
+             child: Text("Get Started")),
+            TextButton(onPressed: (){
+                 Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)  {
+                  return WidgetTree();
+                }));
+            }, 
+             style: FilledButton.styleFrom(
+              minimumSize: Size(double.infinity, 40.0)
+            ),
+            child: Text("Login"))
         ],
         ),
       ),
--- lib/Views/Widgets/hero_widget.dart
+++ lib/Views/Widgets/hero_widget.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
 
 class HeroWidget extends StatelessWidget {
-  const HeroWidget({super.key});
+  final String title;
+
+  const HeroWidget({super.key, this.title = "Your Title"});
 
   @override
   Widget build(BuildContext context) {
-    return  Hero(
-      tag:'hero1',
-      child: ClipRRect(
-                borderRadius: BorderRadius.circular(20.0),
-                child: Image.asset("assets/images/happy.png",color: Colors.teal,colorBlendMode: BlendMode.darken,)//.asset('assets/images/happy.png')
-              ),
+    return Column(
+      children: [
+        Hero(
+          tag: 'hero1',
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image.asset(
+              "assets/images/happy.png",
+              color: Colors.teal,
+              colorBlendMode: BlendMode.darken,
+            ),
+          ),
+        ),
+        const SizedBox(height: 20), // optional spacing
+        FittedBox(
+          child: Text(
+            title,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 50.0,
+              letterSpacing: 5.0,
+              color: Colors.white60 // You had 50.0, which is extremely wide
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
