b7ccdd1e8b2231d8437b6f72de6a81186db872d8
sengendo Nazil <ssengendonazil@gmail>
2025-07-29 15:09:27 +0300

    dialog

--- lib/Views/Pages/home_page.dart
+++ lib/Views/Pages/home_page.dart
@@ -1,43 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:learn_flutter/Views/Widgets/contsiner_widget.dart';
 import 'package:learn_flutter/Views/Widgets/hero_widget.dart';
+import 'package:learn_flutter/data/constants.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    List<String> list = [
+      "moviebox.id",
+      "moviebox.ph",
+      "moviebox.pk",
+      "movieboxapp.in",
+      KValue.keyConcepts,
+      KValue.basicLayout,
+      KValue.cleanUi,
+      KValue.FixBugs
+    ];
     return Padding(
-      padding: const EdgeInsets.all(20.0),
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
       child: SingleChildScrollView(
         child: Column(
           children: [
-             HeroWidget(),
-             ContainerWidget( title: "Title", Description: "Description",),
-             ContainerWidget( title: "Title", Description: "Description",),
-             ContainerWidget( title: "Title", Description: "Description",),
-             ContainerWidget( title: "Title", Description: "Description",),
-             ContainerWidget( title: "Title", Description: "Description",),
-             ContainerWidget( title: "Title", Description: "Description",),
-             ContainerWidget( title: "Title", Description: "Description",),
-             ContainerWidget( title: "Title", Description: "Description",),
-//               
-//=-P09O8790-876543OP[9-08P7O654TREFGDHJKL;KL------------------------------------------------------------
-             ContainerWidget( title: "Title", Description: "Description",),
-             ContainerWidget( title: "Title", Description: "Description",),
-             ContainerWidget( title: "Title", Description: "Description",),
-             ContainerWidget( title: "Title", Description: "Description",),
-             ContainerWidget( title: "Title", Description: "Description",),
-             ContainerWidget( title: "Title", Description: "Description",),
-             ContainerWidget( title: "Title", Description: "Description",),
-//                ------------------------------------------------------------
-             ContainerWidget( title: "Title", Description: "Description",),
-             ContainerWidget( title: "Title", Description: "Description",),
-             ContainerWidget( title: "Title", Description: "Description",),
-             ContainerWidget( title: "Title", Description: "Description",),
-             ContainerWidget( title: "Title", Description: "Description",),
-             ContainerWidget( title: "Title", Description: "Description",),
-             ContainerWidget( title: "Title", Description: "Description",),
+            HeroWidget(),
+            Column(
+              children: List.generate(
+                list.length,
+                (index) =>
+                    ContainerWidget(title: list[index], Description: "Description"),
+              ),
+            ),
           ],
         ),
       ),
--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -3,3 +3,11 @@ class KConstants {
   static const String themeModeKey = 'isDarkMode';
 
 }
+
+class KValue {
+  static const String basicLayout = ' Basic Layout';
+  static const String cleanUi = ' Clean UI';
+  static const String FixBugs = ' Fix Bugs';
+  static const String keyConcepts = ' Key Concepts';
+ 
+}
